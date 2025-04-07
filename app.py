7b04b84af125068fff58d01963655cafd1acedc7
trh4ckn0n <trhacknon@proton.me>
2025-04-07 11:40:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ user_prompt = st.sidebar.text_area("Description du logo", value=user_prompt, key
 
 st.sidebar.markdown("""
 <div style="font-family:'Press Start 2P'; border:1px solid #ff0000; border-radius:8px; padding:16px; margin:16px 0; background-color:#1c1c1e; color:#fff;">
-    <h3>Résumé de vos choix</h3>
+    <h2>Résumé de vos choix</h2><h1>:</h1>
     <ul>
         <li><b>Style</b>: {}</li>
         <li><b>Couleur</b>: {}</li>
@@ -57,7 +57,7 @@ st.markdown("Créons quelque chose de magnifique et puissant ensemble.")
 # st.subheader("Résumé de vos choix")
 st.markdown("""
 <div style="font-family:'Press Start 2P';border:1px solid #ddd; border-radius:8px; padding:16px; margin:16px 0; background-color:#000000; color:#fff;">
-    <h2>Résumé de vos choix</h2><h1> :</h1>
+    <h2>Résumé de vos choix</h2><h1>:</h1>
     <ul>
         <li><b>Style</b>: {}</li>
         <li><b>Couleur</b>: {}</li>
