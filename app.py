8707a3514706a6c264ef9c7ff81268f4bdddc771
trh4ckn0n <trhacknon@proton.me>
2025-04-07 11:45:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,13 @@ def local_css(file_name):
 # Appliquer le CSS personnalisé
 local_css("style.css")
 
+def local_csss(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Appliquer le CSS personnalisé
+local_css("s.css")
+
 # Récupérer les entrées de l'utilisateur depuis helpers.py
 style, color, hidden_word, user_prompt = get_user_inputs()
 
