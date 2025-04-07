85ee8ec11f5f8bddc27801de566ccf1dd31a7f71
trh4ckn0n <trhacknon@proton.me>
2025-04-07 11:48:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def local_css(file_name):
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Appliquer le CSS personnalisé
-local_css("style.css")
+local_css("styles.css")
 
 def local_csss(file_name):
     with open(file_name) as f:
